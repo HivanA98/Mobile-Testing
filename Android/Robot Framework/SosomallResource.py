78c050f3764b72602fd209ab7ff3b81618550d85
Ivan Armadi <103761586+HivanA98@users.noreply.github.com>
2024-09-16 22:10:01 +0700

    UpdateResourceV.1.0.3

--- Android/Robot Framework/SosomallResource.py
+++ Android/Robot Framework/SosomallResource.py
@@ -1,13 +1,16 @@
 from robot.api.logger import info, debug, trace, console
 
-
+#StrorePage
 SearchBtn = "//android.widget.TextView[@text='']"
 SosomallDollar = '//android.widget.TextView[@text="SOSOMALL Dollar"]'
+BackBtn = 'id=00000000-0000-001a-ffff-ffff000000c2'
+MsgBtn = 'id=00000000-0000-001e-ffff-ffff00000163'
 
 #MainButton
-MassageBtn = '//android.view.View[@content-desc="󰆉"]/android.view.ViewGroup'
-CartBtn = '//android.view.View[@content-desc=""]'
-ProfileBtn = 'xpath=//android.view.View[@content-desc=""]/android.view.ViewGroup'
+HomeBtn = '//android.view.View[@content-desc=""]'
+MassageBtn = '//android.view.View[@content-desc="󰆉" and @index="1"]'
+CartBtn = '//android.view.View[@content-desc="" and @index="2"]'
+ProfileBtn = '//android.view.View[@content-desc="" and @index="3"]'
 
 #MainPage
 StorePage = '//android.widget.TextView[@text="商店"]'
@@ -16,10 +19,18 @@ KolPage = '//android.widget.TextView[@text="影片"]'
 #Category
 CPopular = '//android.widget.TextView[@text="熱門"]'
 
+#KolPage
+LikeBtn = 'id=00000000-0000-001e-ffff-ffff0000023e'
+CommentBtn = 'id=00000000-0000-001e-ffff-ffff00000243'
+ShareBtn = 'id=00000000-0000-001e-ffff-ffff00000248'
+OptionBtn = 'id=00000000-0000-001e-ffff-ffff0000024a'
+UploadBtn = 'id=00000000-0000-001e-ffff-ffff0000021a'
+FollowBtn = 'id=00000000-0000-001e-ffff-ffff00000235'
+VendorProfileBtn = 'id=00000000-0000-001e-ffff-ffff0000025a'
+
 #Login
-idBtn = '00000000-0000-0096-ffff-ffff000000ce'
 idbt = 'class=android.widget.EditText'
-PassBtn = '00000000-0000-0096-ffff-ffff000000d2'
+Pss = 'class=android.widget.EditText[1]'
 PassBt = 'xpath=//android.widget.EditText[@text="*******"]'
 SubmitBtn = '//android.view.ViewGroup[@content-desc="登入搶購優惠"]'
 
